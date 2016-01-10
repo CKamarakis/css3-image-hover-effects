c1c54a1c49e7675f91aeba023cf40e9656f9166e
Christodoulos Kamarakis <ck@11dig.it>
2016-01-10 16:53:38 +0200

    #8 - don, zoom in, zoom out, zoom out anchored, zoom out rotate, slide, slide rotate

--- tasks/js/load.js
+++ tasks/js/load.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	$("#greyscale-effects").load("./views/greyscale.html");
 	$("#fade-effects").load("./views/fade.html");
-	$("#zoom").load("./views/zoom.html");
+	$("#zoom-effects").load("./views/zoom.html");
 	$("#shapes-effects").load("./views/shapes.html");
 });
